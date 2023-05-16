106d98774fc0cd86d80b4a1a8666a5b284592621
linli <linli@joinbonnet.com>
2023-05-16 22:59:45 +0100

    init

---
+++ config.py
@@ -0,0 +1,4 @@
+# bonnet-warehouse - datawarehouse
+conn_string = "host='bonnet-warehouse-cluster.cbdcpozilt0r.eu-west-2.rds.amazonaws.com' dbname='datawarehouse' user='linli' password='bkVhVPCyZfdjFrSCmRmAUqUgWgJzrRGKdSBNQVhVudGDhmfGJsZtjQRyRTGemnkP'"
+
+out_conn_string = 'postgresql://linli:AuQXhdxAVFbpNdms@data-warehouse.cbdcpozilt0r.eu-west-2.rds.amazonaws.com:5432/datawarehouse'
---
+++ module/__init__.py
@@ -0,0 +1,1 @@
+from module.pg_conn import pgconnect
